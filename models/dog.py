7d17cbae2d799da379c2a0422f6b878e7cbf5c6b
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-27 00:26:07 +0200

    Adding post endpoint to create a dog object

--- models/dog.py
+++ models/dog.py
@@ -18,3 +18,7 @@ class DogModel(db.Model):
     @classmethod
     def get_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
--- resources/dog.py
+++ resources/dog.py
@@ -4,7 +4,6 @@ from models.dog import DogModel
 
 class Dog(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name', type=str, required=True, help='Doggo needs a name')
     parser.add_argument('breed', type=str, required=True, help='Doggo needs a breed')
 
     def get(self, name):
@@ -15,7 +14,18 @@ class Dog(Resource):
         return {'message': 'Doggo not found'}, 404
 
     def post(self, name):
-        pass
+        if DogModel.get_by_name(name):
+            return {'message': '"{}" already exists'.format(name)}, 400
+
+        data = Dog.parser.parse_args()
+        dog = DogModel(name, data['breed'])
+
+        try:
+            dog.save_to_db()
+        except:
+            return {'message': 'An error occurred saving doggo'}, 500
+
+        return dog.json(), 201
 
     def put(self, name):
         pass
