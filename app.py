2b07f388a4ae63f7aa145f32bd8b9191a2aa4269
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-26 23:31:49 +0200

    Implementing sqlite db and defining schema

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
+from db import db
+from resources.dog import Dog, DogList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 
-@app.route('/')
-def home():
-    return 'Hello world'
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
-app.run(port=5000)
+api.add_resource(Dog, '/dog/<string:name>')
+api.add_resource(DogList, '/dogs')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
