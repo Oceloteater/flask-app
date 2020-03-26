4304ac29846b28791ecd65a4f9ee98e2c5148b74
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-26 22:25:17 +0200

    Creating db instance and initial dog model

---
+++ models/dog.py
@@ -0,0 +1,16 @@
+from db import db
+
+
+class DogModel(db.Model):
+    __tablename__ = 'dogs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    breed = db.Column(db.String(80))
+
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def json(self):
+        return {'name': self.name, 'breed': self.breed}
